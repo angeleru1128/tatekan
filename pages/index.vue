a417a4c2a5b2725bc1a293a937a7990b267b2720
angeleru1128 <82640338+angeleru1128@users.noreply.github.com>
2021-05-23 19:45:54 +0900

    feature/ayaka/index.vue変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,31 @@
 <template>
+<div class = 'components'>
+  <SchoolImage />
+  <UploadButton />
+  <DetailInput />
+
+</div>
 </template>
 
+
+
+
 <script>
+import SchoolImage from '@/components/SchoolImage'
+import UploadButton from '@/components/imageInput'
+import DetailInput from '@/components/detailInput'
+
+export default {
+  components: {
+    SchoolImage,
+    UploadButton,
+    DetailInput,
+  }
+}
 </script>
+
+<style>
+.components {
+  background-color: #FFFFFF;
+}
+</style>
