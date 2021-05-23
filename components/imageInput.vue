41caad3cc843c339a9fffa5a85d2a01169734cbc
angeleru1128 <82640338+angeleru1128@users.noreply.github.com>
2021-05-23 17:05:28 +0900

    feature/ayaka/アップロードボタンを追加

---
+++ components/imageInput.vue
@@ -0,0 +1,26 @@
+<template>
+ <div>
+  <a href="#" class="UploadButton" role="button">Upload</a>
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style> 
+.UploadButton {
+  background-color: #00a656;
+  border-radius: 1.5em;
+  box-shadow: 0 0.2em 0.5em rgba(0, 0, 0, 0.2);
+  padding: 1em 2em;
+  color: #ffffff;
+  font-weight: bold;
+  text-decoration: none;
+  position: relative;
+  left: 1000px;
+  bottom: 30px;
+}
+</style>
