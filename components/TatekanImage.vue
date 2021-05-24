6b01061174988e5ce7e69c4a01274ea68abd2f7c
thistle519 <shizuru519@gmail.com>
2021-05-24 17:43:53 +0900

    画像にまつわるJSを書いている

--- components/TatekanImage.vue
+++ components/TatekanImage.vue
@@ -1,8 +1,24 @@
 <template>
     <div>
-        <img src="tatekan_images/demo.jpg">
+        <img :src="'tatekan_images/${event.image_name}'">
     </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      demo: {}
+    }
+  },
+  created() {
+    const params = this.$nuxt.$route.params
+    const eventsInfo = require('../../builds/events-info')
+    const event = eventsInfo.events[params.event]
+    this.event = event
+  }
+}
+</script>
+
 <style scoped>
 /* img{
      position: absolute;
