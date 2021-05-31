8e73f1d90699c6325274943bf684a49c5f587387
thistle519 <shizuru519@gmail.com>
2021-05-31 17:26:54 +0900

    アイコンをマップ上にとにかく配置

--- components/SchoolImage.vue
+++ components/SchoolImage.vue
@@ -1,23 +1,27 @@
 <template>
- <div>
-  <img src="tsukubamap.png">
- </div>
+  <div style="position: relative;">
+    <img src="tsukubamap.png" usemap="#tsukuba" />
+    <map name="tsukuba">
+      <div class="icon" style="position:absolute; top : 105px; left : 210px;">
+        <area style="color: #00ff00;"
+          href="http://www.cman.jp"
+          shape="circle"
+          alt="円形"
+          coords="210,105,40"
+        />
+        <img src="/tatekan_images/icon.png" width="12" />
+      </div>
+    </map>
+  </div>
 </template>
 
-
 <script>
-export default {
-
-}
+export default {};
 </script>
-
 
 <style scoped>
 img {
   position: relative;
-  /* top: 180px;
-  left: 10px; */
-
-  
 }
+
 </style>
