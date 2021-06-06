cbb39b29c2a35d4eb6515483daf8ce0d43de67ed
angeleru1128 <82640338+angeleru1128@users.noreply.github.com>
2021-06-06 22:11:31 +0900

    x,y 入力変更

--- components/detailInput.vue
+++ components/detailInput.vue
@@ -1,10 +1,8 @@
 <template>
  <div>
-  <label for="xy">x,y　入力</label>
+  <label for="xy">x,y 入力</label>
   <input type="number" name="number" max="50" value="15">
-  <input type="number" name="number" max="50" value="51">
-
-  
+  <input type="number" name="number" max="50" value="15">
  </div>
 </template>
 
@@ -15,33 +13,17 @@ export default {
 }
 </script>
 
-
-
 <style scoped>
-.label {
+label {
     color: #000000;
     font: 1rem 'Fira Sans', sans-serif;
+
 }
 
+/* x,y入力する場所の幅 */
 input {
-    border: 2px solid #000000;
+    border: 1px solid #000000;
     width: 15%;
 }
 
-
-label,
-input {
-  margin: .4rem 0;
-  color: #000000;
-  position: relative;
-  left: 600px;
-  bottom: 40px;
-}
-
-
-
 </style>
-
-
-
- 
