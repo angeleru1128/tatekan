dea16520594d4112640239040d97d4be8211ec6c
thistle519 <shizuru519@gmail.com>
2021-06-08 22:09:53 +0900

    動的に取れるようにした

--- components/TatekanImage.vue
+++ components/TatekanImage.vue
@@ -1,11 +1,21 @@
 <template>
-    <div>
-        <img src=tatekan_images/demo.jpg>
-    </div>
+  <div>
+    <img :src="tatekan_image" />
+  </div>
 </template>
 <script>
+export default {
+  props: {
+    tatekan_image: {
+      type: String,
+      required: true
+    }
+  }
+};
 </script>
 
 <style scoped>
-
+img {
+  /*height: 520px;*/
+}
 </style>
