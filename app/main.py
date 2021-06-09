6e68eee7686e56234389c0dba006451d77c2b094
Akei Kanzaki <tom0pr01fr.olem.001@gmail.com>
2021-06-09 21:52:37 +0900

    fuwafuwa

--- app/main.py
+++ app/main.py
@@ -55,4 +55,4 @@ def top_page():
   return TD.TatekanData.getTopData()
 
 if __name__ == '__main__':                        
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
